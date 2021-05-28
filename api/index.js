32f1dffa124459a7f0947c0071495cfd48b7f4ec
Afaizin-bitt <84916753+Afaizin-bitt@users.noreply.github.com>
2021-05-28 22:01:28 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,17 +16,46 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict to main predict`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
+bot.onText(/\/predict/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
-    );   
+        `masukkan nilai i|v seperti 2/2`
+    );
+    state = 1
 });
+
+bot_on('message', (msg)=> {
+    if(state == 1){
+        console.log(msg.text);
+        s = msg.text.split("|")
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
+            ]
+    ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                'Nilai V yang diprediksi adalah S{jres[0]} Volt
+                );
+            bot.sendMessage(
+                msg.chat.id,
+                'Nilai P yang diprediksi adalah S{jres[0]} Watt
+                );
+        })
+    }else{
+    state = 0
+}
+       })
+                
+
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
